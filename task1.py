a3e6cdcf9912ad6657c2534eeea300603d3b5301
olehpidhi <olehpid@gmail.com>
2015-09-20 20:36:37 +0300

    Fixed Newton method

--- task1.py
+++ task1.py
@@ -12,25 +12,30 @@ def df(x):
 def secant(f, a, b):
     return (a * f(b) - b * f(a)) / (f(b) - f(a))
 
-def newton(f, x, dumb = 0):
+def newton(f, dumb, x):
     return x - f(x)/df(x)
 
 def combined(f, a, b):
-    x = newton(f,a)
+    x = newton(f, a, b)
     return secant(f, x, b)
 
-def solve(func, epsilon, a, b = 0, iteration = 0, method = newton):
+def solve(func, epsilon, a = 0, b = 0, iteration = 0, method = newton):
     x = method(func, a, b)
     while abs(x-b) > epsilon:
         b = x
         iteration += 1
-        x = secant(func, a, b)
+        x = method(func, a, b)
     return x, iteration
 
 print("secant")
 print(solve(func = f, epsilon = 0.0000000001, a = -2.95, b = -2.8, method = secant))
 print("newton")
-print(solve(func = f, epsilon = 0.0000000001, a = -2.95, method = newton))
+print(solve(func = f, epsilon = 0.0000000001, b = -2.8, method = newton))
 print("combined")
 print(solve(func = f, epsilon = 0.0000000001, a = -2.95, b = -2.8, method = combined))
 
+def f1(x):
+    return math.sin(x + 0.5) - 1
+
+def g1(y):
+    return -math.cos(y - 2)
